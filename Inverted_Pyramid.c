5b8831b947dfe2f602d72c185bad85a1916dff91
VismayVora <77540751+VismayVora@users.noreply.github.com>
2021-09-23 20:58:21 +0530

    Create Inverted_Pyramid.c

---
+++ Inverted_Pyramid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number of rows:");
+    scanf("%d",&n);
+    
+    for(int i = n; i >= 1; i--)
+    {
+        for (int j = n-i; j >=1 ; j--)
+        {
+            printf(" ");
+        }
+        for (int k = i; k >= 1; k--)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
