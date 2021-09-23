e24770ac7e74c3f854a7c4a1331e45437319f3c8
VismayVora <77540751+VismayVora@users.noreply.github.com>
2021-09-23 20:56:07 +0530

    Create Pyramid.c

---
+++ Pyramid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number of rows:");
+    scanf("%d",&n);
+    
+    for(int i = 1; i<=n; i++)
+    {
+        for (int j=1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+        for (int k=1; k <= i; k++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
